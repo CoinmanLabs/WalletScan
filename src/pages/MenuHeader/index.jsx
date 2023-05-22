acf6bcabe74a0af4e3d514c01000d1a827967ef9
CoinmanLabs <110964885+CoinmanLabs@users.noreply.github.com>
2023-05-22 13:10:33 +0800

    Update index.jsx

--- src/pages/MenuHeader/index.jsx
+++ src/pages/MenuHeader/index.jsx
@@ -43,20 +43,7 @@ const MenuHeader = () => {
             label: 'Deposit',
             key: 'deposit',
         },
-        {
-            label: 'Coffee',
-            key: 'coffee',
-        },
-        {
-            label: <a href="https://github.com/wxtsky/MyWalletScan" target="_blank"
-                      rel="noopener noreferrer"><GithubOutlined/></a>,
-            key: 'github',
-        },
-        {
-            label: <a href="https://twitter.com/jingluo0" target="_blank"
-                      rel="noopener noreferrer"><TwitterOutlined/></a>,
-            key: 'twitter',
-        },
+       
         {
             label: <EthPrice/>,
             key: 'ethPrice',
